2af11aa1a6ddf995689c182f582599e415821302
LochieCameron <lochiecameron@gmail.com>
2014-06-14 22:28:36 +1000

    New!

---
+++ if_else.rb
@@ -0,0 +1,7 @@
+#Code Academy
+
+if 1 > 2
+  print "I won't get printed because one is less than two."
+else
+  print "That means I'll get printed!"
+end
---
+++ not.rb
@@ -0,0 +1,10 @@
+# boolean_1 = !true
+boolean_1 =  !true
+
+# boolean_2 = !true && !true
+boolean_2 = !true
+
+# boolean_3 = !(700 / 10 == 70)
+boolean_3 = !true
+
+# Code Academy
